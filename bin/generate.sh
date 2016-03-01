4d2f397532e36abc98095d946108a1b2bbc448fa
PavelD <konference@lnx.cz>
2016-03-01 15:20:17 +0100

    * Fix encoding * Add version info for future usage

--- bin/generate.sh
+++ bin/generate.sh
@@ -14,20 +14,22 @@ for file in $files; do
       if [ "$relId" != '#' ]; then
         wget -qO- http://polygons.openstreetmap.fr/index.py?id=$relId > ../export/$relId.status
 
-        nameOriginal=$(wget -qO- http://www.openstreetmap.org/relation/$relId | grep -A1 Key:name\? | grep browse-tag-v | awk -F'>|<' '{print $3}' | recode utf-8..html | sed 's/\&amp\;/\&/g' | recode html..utf-8 )
+        wget -qO- http://www.openstreetmap.org/relation/$relId > "$f/$relId.txt"
+        nameOriginal=$(cat "$f/$relId.txt" | grep -A1 Key:name\? | grep browse-tag-v | awk -F'>|<' '{print $3}' | recode utf-8..html | sed 's/\&amp\;/\&/g' | recode html..utf-8 )
         name=$(echo $nameOriginal | iconv -f utf-8 -t ascii//translit | tr "\/" "--" )
+        changeset=$(cat "$f/$relId.txt" | grep changeset/ | awk -F'>|<' '{print $3}')
 
-        echo "#@$nameOriginal" > "$f/$relId.txt"
-        echo "# This polygon is based on data © OpenStreetMap contributors" >> "$f/$relId.txt"
+        echo "#@$nameOriginal" | iconv -f utf-8 -t windows-1252//translit > "$f/$relId.txt"
+        echo "# This polygon is based on data © OpenStreetMap contributors" |  iconv -f utf-8 -t windows-1252//translit >> "$f/$relId.txt"
         echo "# The OpenStreetMap data is made available under the Open Database License, see http://www.openstreetmap.org/copyright" >> "$f/$relId.txt"
         echo "# This polygon file is made available under the same Open Database License: http://opendatacommons.org/licenses/odbl/1.0/." >> "$f/$relId.txt"
-        echo "# relation Id: $relId" >> "$f/$relId.txt"
+        echo "# relation Id: $relId; changeset: $changeset" >> "$f/$relId.txt"
         echo >> "$f/$relId.txt"
         wget -qO- http://polygons.openstreetmap.fr/get_poly.py?id=$relId\&params=0 | grep -F . | awk '{$1=$1;print}' >> "$f/$relId.txt"
         echo >> "$f/$relId.txt"
 
-        sed -i 's/$'"/`echo \\\r`/" "$f/$relId.txt"
-        mv "$f/$relId.txt" "$f/${name}_$relId.txt"
+        sed -i 's/$'"/`echo \\\r`/" "$f/${relId}.txt"
+        mv "$f/${relId}.txt" "$f/${name}_$relId.txt"
         mv ../export/$relId.status "../export/$fdir ${name}_$relId.html"
         echo "$relId: $name done";
       fi
